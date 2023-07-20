7c428c0ff4200f871caa0175f9a6f5deaec0e727
Sam Zog <samyzogeyb@gmail.com>
2023-07-21 01:33:54 +0300

    tweak notification permission request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,40 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const showNotification = () => {
+    navigator?.serviceWorker?.ready?.then?.((registration) => {
+      registration?.active?.postMessage?.({
+        title: "Hello",
+        body: "Hello, world!",
+      });
+    });
+  };
+
   return (
     <div className="p-10">
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={async () => {
-          Notification.requestPermission((permission) => {
-            if (permission === "granted") {
-              navigator?.serviceWorker?.ready?.then?.((registration) => {
-                registration?.active?.postMessage?.({
-                  title: "Hello",
-                  body: "Hello, world!",
-                });
+          try {
+            Notification.requestPermission().then((permission) => {
+              if (permission === "granted") {
+                showNotification();
+              }
+            });
+          } catch (error) {
+            // Safari doesn't return a promise for requestPermissions and it
+            // throws a TypeError. It takes a callback as the first argument
+            // instead.
+            if (error instanceof TypeError) {
+              Notification.requestPermission((permission) => {
+                if (permission === "granted") {
+                  showNotification();
+                }
               });
+            } else {
+              throw error;
             }
-          });
+          }
         }}
       >
         show notfication
