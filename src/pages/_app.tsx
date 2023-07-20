369da8dadbeb3f9a1c37a9ba6e1186bada2cbac3
Sam Zog <samyzogeyb@gmail.com>
2023-07-21 00:16:23 +0300

    console log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
       return;
     }
 
+    console.log(Notification.permission);
+
     if (Notification.permission === "granted") {
       registerSW();
     }
