7c0f82f552021b5f34f65326fbab3a4045bb405b
Sam Zog <samyzogeyb@gmail.com>
2023-07-21 13:13:05 +0300

    register worker on load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,31 +5,7 @@ import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if (!("Notification" in window)) {
-      return;
-    }
-
-    console.log(Notification.permission);
-
-    if (Notification.permission === "granted") {
-      registerSW();
-    }
-
-    if (Notification.permission !== "denied") {
-      try {
-        Notification.requestPermission().then((permission) => {
-          if (permission === "granted") {
-            registerSW();
-          }
-        });
-      } catch (error) {
-        Notification.requestPermission((permission) => {
-          if (permission === "granted") {
-            registerSW();
-          }
-        });
-      }
-    }
+    registerSW();
   }, []);
 
   return <Component {...pageProps} />;
